e61b1a20af0f201fc8cb611d9e1389a91db8d672
samuelefab17 <samuelefab17@gmail.com>
2024-05-21 16:56:54 +0200

    Web_Server_PCTOv3
    Creation of the webserver with access point that hosts a web page written in test html

--- Web_Server_PCTO.ino
+++ Web_Server_PCTO.ino
@@ -1,9 +1,36 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <WiFi.h>
+#include <WiFiClient.h>
+#include <WebServer.h>
 
+#include "index.h" 
+
+WebServer server(80);
+
+const char* ssid = "FORZAFERRARISEMPREE";
+const char* password = "vesrtappenamuroesigode";
+
+void handleRoot() {
+ String html = main_html; 
+ server.send(200, "text/html", html); 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void setup(void){
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
 
+  WiFi.mode(WIFI_AP); 
+  WiFi.softAP(ssid, password);    
+
+  Serial.println(WiFi.localIP());  
+ 
+  server.on("/", handleRoot);     
+ 
+  server.begin();                 
+  Serial.println("HTTP server started");
+}
+
+void loop(void){
+  server.handleClient();
+  delay(1);
 }
---
+++ index.h
@@ -0,0 +1,17 @@
+const char main_html[] PROGMEM = R"=====(
+<!DOCTYPE html>
+<html>
+<head>
+    <title>prova</title>
+    <style>
+        body {
+            background-color: #fcfcc9; 
+        }
+    </style>
+</head>
+<body>
+    <p><h1>prova</h1></p>
+    
+</body>
+</html>
+)=====";
