942526948c941f30993746f1a50dc1dd8d26f32f
samuelefab17 <samuelefab17@gmail.com>
2024-05-23 15:40:10 +0200

    Web_Server_PCTOv6
    A graph was introduced into the html page as a test

--- index.h
+++ index.h
@@ -9,7 +9,7 @@ const char* webpage = R"=====(
 </head>
 <body>
     <h1>ESP32 Test</h1>
-    <p>Rad: <span style="color: red;"><span id="Rad"></span></p>
+    <p>Rad: <span style="color: Rad;"><span id="Rad"></span></p>
     <script>
         function fetchRad() {
             fetch("/Rad")
@@ -21,7 +21,32 @@ const char* webpage = R"=====(
 
         fetchRad();
         setInterval(fetchRad, 1000); // Update temperature every 4 seconds
+</script>
+<p></p>
+<p></p>
+<canvas id="myCanvas" width="800" height="800"></canvas>
+<script>
+var canvas = document.getElementById("myCanvas"); // Seleziona l'elemento canvas
+		var ctx = canvas.getContext("2d"); // Seleziona il contesto 2D
+
+		var dati = [12, 19, 3, 5, 2, 3, 7]; // Dati per il grafico
+		var labels = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio"]; // Etichette sull'asse x
+
+		// Disegna il grafico
+		ctx.beginPath();
+		ctx.moveTo(0, 0);
+		for (var i = 0; i < dati.length; i++) {
+			var x = i * 60;
+			var y = 600 - dati[i] * 10;
+			ctx.lineTo(x, y);
+			ctx.fillText(labels[i], x, 600);
+			ctx.arc(x, y, 5, 0, Math.PI*2);
+			ctx.fill();
+		}
+		ctx.stroke();
     </script>
+    
+
 </body>
 </html>
 )=====";
